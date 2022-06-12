1795a7161d7e2335e70ed6561b0f94c90c8b2c03
F1704738 Caio Cavalcanti de Aguiar Castro <caio.castro@bb.com.br>
2022-06-12 17:33:25 -0300

    Added logic for switching pages on topbar

--- src/components/Topbar/index.tsx
+++ src/components/Topbar/index.tsx
@@ -1,4 +1,9 @@
-import { useNavigate, BrowserRouterProps } from "react-router-dom";
+import {
+  useNavigate,
+  BrowserRouterProps,
+  useResolvedPath,
+  useLocation,
+} from "react-router-dom";
 import UserAvatar from "./UserAvatar";
 
 import { Container, Content, TabButton, BottomSpacer } from "./styles";
@@ -15,6 +20,7 @@ type State = {} & ReturnType<typeof mapStateToProps>;
 
 function Topbar(props: State) {
   const navigate = useNavigate();
+  const navPath = useLocation();
 
   const onNavClick = (path: string) => {
     navigate(path);
@@ -24,12 +30,30 @@ function Topbar(props: State) {
     <>
       <Container>
         <Content>
-          <TabButton onClick={() => onNavClick("/")}>Inicio</TabButton>
-          <TabButton onClick={() => onNavClick("/textos")}>Textos</TabButton>
-          <TabButton onClick={() => onNavClick("/servicos")}>
+          <TabButton
+            onClick={() => onNavClick("/")}
+            selected={navPath.pathname === "/" ? true : false}
+          >
+            Inicio
+          </TabButton>
+          <TabButton
+            onClick={() => onNavClick("/textos")}
+            selected={navPath.pathname === "/textos" ? true : false}
+          >
+            Textos
+          </TabButton>
+          <TabButton
+            onClick={() => onNavClick("/servicos")}
+            selected={navPath.pathname === "/servicos" ? true : false}
+          >
             Serviços
           </TabButton>
-          <TabButton onClick={() => onNavClick("/sobre")}>Sobre</TabButton>
+          <TabButton
+            onClick={() => onNavClick("/sobre")}
+            selected={navPath.pathname === "/sobre" ? true : false}
+          >
+            Sobre
+          </TabButton>
         </Content>
         <UserAvatar user={props.loggedUser} />
       </Container>
